e8607f00ae270c81b5851b132eb618a561463315
박상민 <a01071397193@gmail.com>
2024-12-21 21:42:23 +0900

    !HOTFIX :: navigation 에러

--- src/navigations/RootNavigation.tsx
+++ src/navigations/RootNavigation.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import {createStackNavigator} from "@react-navigation/stack";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
 
 import Login from "@/screens/auth/login";
+import SignUp from "@/screens/auth/signup";
+import Hospital from "@/screens/hospital";
 
 export type RootStackPramList = {
     Login: undefined;
+    SignUp: undefined;
+    Hospital: undefined;
 }
 
-const Stack = createStackNavigator<RootStackPramList>();
+const Stack = createNativeStackNavigator<RootStackPramList>();
 
 const RootNavigation = () => {
     return (
         <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="Hospital" component={Hospital} />
         </Stack.Navigator>
     )
 }
