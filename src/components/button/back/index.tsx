71712af0f87fa74b3452759361da4ea0d1701e22
박상민 <a01071397193@gmail.com>
2024-12-21 23:11:06 +0900

    Feat :: BackButton 컴포넌트화

---
+++ src/components/button/back/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {TouchableOpacity, StyleSheet, ViewStyle} from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+import {Theme} from "@/design/theme";
+
+type BackButtonProps = {
+    styles?: ViewStyle;
+    onPress?: () => void;
+}
+
+const BackButton = ({styles, onPress}: BackButtonProps) => {
+    const navigation = useNavigation();
+
+    // 기본 스타일과 전달받은 스타일을 병합하고, 전달받은 스타일이 없으면 기본 스타일만 사용하는 코드
+    const combinedStyles = StyleSheet.flatten([defaultStyles.backButton, styles || {}]);
+
+    return (
+        <TouchableOpacity style={combinedStyles} onPress={onPress || (() => navigation.goBack())}>
+            <Feather name="arrow-left" size={24} color="black"/>
+        </TouchableOpacity>
+    );
+};
+
+const defaultStyles = StyleSheet.create({
+    backButton: {
+        color: Theme.colors.gray[300],
+        position: 'absolute',
+        top: 50,
+        left: 20,
+        padding: 8,
+        zIndex: 10,
+    },
+});
+
+export default BackButton;
